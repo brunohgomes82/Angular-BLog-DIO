e232ab84cd5c984bde7c9258bcb6464266d2c5b6
brunohgomes <brunohgomes82@gmail.com>
2023-10-06 12:14:11 -0300

    projet test created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,27 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title": "TITULO ILUSTRATIVO!!!",
+    "description": "Testando a funcionalidade responsiva",
+    "photoCover": "https://static.grainger.com/rp/s/is/image/Grainger/34JR18_AS01"
+  },
+  {
+    "id":"2",
+    "title": "Titulo Responsivo",
+    "description": "ola bla bla bla",
+    "photoCover": "https://i.pinimg.com/originals/b8/3d/49/b83d492de574050ede36bfb510915a7f.png"
+  },
+  {
+    "id":"3",
+    "title": "Titulo Responsivo",
+    "description": "ola bla bla bla",
+    "photoCover": "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.MDopgWisUQuDwcT9mzgyawHaEK%26pid%3DApi&f=1&ipt=9296b26f38a8ba99e0f2055218aa4cab513e2eee2926984bac0704d0b5a7c366&ipo=images"
+  },
+  {
+    "id":"4",
+    "title": "Titulo Responsivo",
+    "description": "ola bla bla bla",
+    "photoCover": "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.explicit.bing.net%2Fth%3Fid%3DOIP.4gmr31HPUfy6C8LN5-nIvwHaEK%26pid%3DApi&f=1&ipt=c233b726654eea9a490e5169e442dcfeb0acb0c15e2812e362353f9d5d5578b8&ipo=images"
+  },
+
+]
